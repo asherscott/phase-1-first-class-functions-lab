131dbbb20804059665e8303c1f6f5d90bdff89b7
asher scott <asher.scott6@gmail.com>
2022-02-23 15:55:55 -0600

    commit tree 3d1a33188876119792ee50a176dbee560dd70432

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Code your solution in this file!
+
+const returnFirstTwoDrivers = function(drivers) {
+    return drivers.slice(0, 2);
+}
+
+const returnLastTwoDrivers = function(drivers) {
+    return drivers.slice(-2);
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+function createFareMultiplier(fare) {
+    return function() {
+        return fare ** 2;
+    }
+}
+
+const fareDoubler = fare => fare * 2;
+
+const fareTripler = fare => fare * 3;
+
+function selectDifferentDrivers(drivers, array) {
+    return array(drivers);
+}
